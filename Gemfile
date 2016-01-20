ccca3303a55373798972d199609778c639fe5d13
sebtn <tonomudo@gmail.com>
2016-01-20 02:24:25 +0000

    Logo social footer

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'bootstrap-sass', '~> 3.3.4'
 
 gem 'sprockets', '2.11.0'
 
+gem 'jquery-ui-rails'
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,11 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap
+//= require jquery-ui
 
 
