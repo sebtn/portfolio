fc91f920bf173f23c87bf82b0ea876bbec3254b0
sebtn <tonomudo@gmail.com>
2016-03-01 01:11:10 -0600

    Improved local host by concat some elements in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Portf::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.precompile += %w( vendor.js vendor.scss )
   # Code is not reloaded between requests.
   config.cache_classes = true
 
